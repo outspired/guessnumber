8466547ff5786b4237ccb99490ef60cce59295d8
putspired <outspired@gmail.com>
2018-09-04 20:48:56 +0300

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import javax.swing.text.DefaultEditorKit;
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,28 +11,74 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println(myNum);
-        System.out.println("Guess the number from 1 to 100!");
-        System.out.println("===============================");
+        String playAgain;
+        do {
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("You have " + (3-i) + " tries left. GO:");
-            int userNum = scan.nextInt();
-            if (myNum > userNum) {
-                System.out.println("The number is greater.");
-                System.out.println("-------------------------------");
-            } else if (myNum < userNum){
-                System.out.println("The number is smaller.");
-                System.out.println("-------------------------------");
-            } else {
-                System.out.println("+++++++++++++++++++++++++++++++");
-                System.out.println("BINGO!");
-                return;
+            int myNum = rand.nextInt(100) + 1;
+            int numberOfTries = 5;
+            System.out.println(myNum);
+            System.out.println("Guess the number from 1 to 100!");
+            System.out.println("===============================");
+            boolean looser = true;
+            for (int i = 0; i < numberOfTries; i++) {
+                System.out.println("You have " + (numberOfTries - i) + " tries left.");
+                //int userNum = scan.nextInt();
+                int userNum = askNum();
+                if (myNum > userNum) {
+                    System.out.println("The number is greater.");
+                    System.out.println("-------------------------------");
+                } else if (myNum < userNum) {
+                    System.out.println("The number is smaller.");
+                    System.out.println("-------------------------------");
+                } else {
+                    System.out.println("+++++++++++++++++++++++++++++++");
+                    System.out.println("BINGO!");
+                    looser = false;
+                    //return;
+                    break;
+                }
+
             }
+            if (looser) {
+                System.out.println("You loose!");
+                System.out.println("The number is " + myNum);
+            }
+            System.out.println("///////////////////////////////");
+            System.out.println("Do you want to play again? y/n");
+            playAgain = askYN();
+        } while (playAgain.equalsIgnoreCase("y"));
+        System.out.println("BYE");
+    }
+    static String askYN() {
+        String answer;
+        do {
+            answer = scan.next();
+            if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
+                System.out.println("Answer y or n!");
+                continue;
+            } else {
+                return answer;
+            }
+        } while (true);
+    }
 
-        }
-        System.out.println("You loose!");
-        System.out.println("The number is " + myNum);
+    static int askNum() {
+        System.out.println("Please try to guess the number:");
+        int userNum;
+        do {
+            try {
+                userNum = scan.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Please enter the number");
+                scan.next();
+                continue;
+            }
+            if (userNum > 0 && userNum < 101) {
+                return userNum;
+            } else {
+                System.out.println("The number should from 1 to 100. Try again:");
+                continue;
+            }
+        } while (true);
     }
 }
